acca86e298a8467f6d2ac38a2742055c8889707a
Luiz Carlos Kazuyuki Fukaya <luizcarloskazuyukifukaya@gmail.com>
2025-01-31 21:58:28 +0900

    Update shell script

--- check_manifest.sh
+++ check_manifest.sh
@@ -5,3 +5,6 @@ TARGET_MANIFEST_FILE="/mnt/c/Users/i_lov/Downloads/manifest.csv"
 
 # Print the variable
 python3 check_object_presense.py $TARGET_MANIFEST_FILE
+
+# How to execute this bash script while outputing to the standard I/O
+#bash this_script.sh | tee -a output.log
